9e8a39e8b685943e81dba3c7726329732a0facfd
BenMunson1 <100311728+BenMunson1@users.noreply.github.com>
2025-06-10 17:16:16 -0500

    Update top_hosts.sql

--- queries/top_hosts.sql
+++ queries/top_hosts.sql
@@ -9,3 +9,17 @@ GROUP BY
 ORDER BY
     total_listings DESC
 LIMIT 10;
+
+/*
+Results:
+219517861	Sonder (NYC)	(327)
+107434423	Blueground	(232)
+30283594	Kara	(121)
+137358866	Kazuya	(103)
+12243051	Sonder	(96)
+16098958	Jeremy & Laura	(96)
+61391963	Corporate Housing	(91)
+22541573	Ken	(87)
+200380610	Pranjal	(65)
+1475015	    Mike	(52)
+*/
